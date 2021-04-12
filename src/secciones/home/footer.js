fff0755aa9e80c4f387d6b0b36bea30bf4103e16
Tom <tomas@indicius.com>
2021-04-12 11:04:06 -0300

    Footer / Agregamos grilla de Bootstrap

--- src/secciones/home/footer.js
+++ src/secciones/home/footer.js
@@ -1,9 +1,9 @@
-import Logo from "../../assets/icons/icon-logo.inline.svg";
+import Logo from '../../assets/icons/icon-logo.inline.svg'
 
 const Footer = () => {
   return (
     /*
-      JUANSO
+      RESUELTO ✅
       1) En HTML, los footers tienen una etiqueta dedicada <footer></footer>. Usémosla.
 
       2) También, fijate que estás "envolviendo" al footer dentro del <container></container>,
@@ -18,42 +18,38 @@ const Footer = () => {
 
     <footer className="footer">
       <div className="container">
-        <div>
-          <img src={Logo} alt="image" width="106px" height="16px"></img>
-          <h3 className="titulo">EN CADA PLATO UNA EMOCION</h3>
-          <button className="button-primary">HACER RESERVA</button>
-        </div>
-        <div>
-          <div>
-            <p>
-              Talcahuano 6960 (S3004JEL) Santa Fe, Provincia de Santa Fe,
-              Argentina
-            </p>
-            <p>+54 9 3425 66-2175</p>
-            <p>contacto@margot.com</p>
+        <div className="row justify-content-between">
+          <div className="col-12 col-sm-6 col-md-5">
+            <img src={Logo} alt="image" width="106px" height="16px"></img>
+            <h3 className="titulo">EN CADA PLATO UNA EMOCION</h3>
+            <button className="button-primary">HACER RESERVA</button>
           </div>
-          <div>
-            <ul className="media">
-              <li>
-                <a href="https://www.instagram.com/margot_e.c/?hl=en">
-                  INSTAGRAM
-                </a>
-              </li>
-              <li>
-                <a href="https://www.facebook.com/margotrestostafe/">
-                  FACEBOOK
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
 
-        <div className="brand">
-          <p>© MARGOT 2021</p>
+          <div className="direccion col-12 col-sm-6 col-md-5">
+            <div>
+              <p>Talcahuano 6960 (S3004JEL) Santa Fe, Provincia de Santa Fe, Argentina</p>
+              <p>+54 9 3425 66-2175</p>
+              <p>contacto@margot.com</p>
+            </div>
+            <div>
+              <ul className="media">
+                <li>
+                  <a href="https://www.instagram.com/margot_e.c/?hl=en">INSTAGRAM</a>
+                </li>
+                <li>
+                  <a href="https://www.facebook.com/margotrestostafe/">FACEBOOK</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="brand col-12">
+            <p>© MARGOT 2021</p>
+          </div>
         </div>
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
