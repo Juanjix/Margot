8d55cc57b1963ff8bd534268a993881591c4db72
Tom <tomas@indicius.com>
2021-04-12 09:57:47 -0300

    CODE REVIEW / Footer

--- src/secciones/home/footer.js
+++ src/secciones/home/footer.js
@@ -1,7 +1,20 @@
-import Logo from "../../assets/icons/icon-logo.inline.svg";
+import Logo from '../../assets/icons/icon-logo.inline.svg'
 
 const Footer = () => {
   return (
+    /*
+      JUANSO
+      1) En HTML, los footers tienen una etiqueta dedicada <footer></footer>. Usémosla.
+
+      2) También, fijate que estás "envolviendo" al footer dentro del <container></container>,
+      y esto va a hacer que tu footer tenga un ancho máximo. Tu <container></container> debería estar dentro de la etiqueta <footer></footer> de esta manera:
+
+      <footer className="footer">
+        <div className="container">
+          [el contenido de tu footer]
+        </div>
+      </footer>
+    */
     <div className="container">
       <div className="footer">
         <div>
@@ -12,24 +25,17 @@ const Footer = () => {
 
         <div>
           <div>
-            <p>
-              Talcahuano 6960 (S3004JEL) Santa Fe, Provincia de Santa Fe,
-              Argentina
-            </p>
+            <p>Talcahuano 6960 (S3004JEL) Santa Fe, Provincia de Santa Fe, Argentina</p>
             <p>+54 9 3425 66-2175</p>
             <p>contacto@margot.com</p>
           </div>
           <div>
             <ul className="media">
               <li>
-                <a href="https://www.instagram.com/margot_e.c/?hl=en">
-                  INSTAGRAM
-                </a>
+                <a href="https://www.instagram.com/margot_e.c/?hl=en">INSTAGRAM</a>
               </li>
               <li>
-                <a href="https://www.facebook.com/margotrestostafe/">
-                  FACEBOOK
-                </a>
+                <a href="https://www.facebook.com/margotrestostafe/">FACEBOOK</a>
               </li>
             </ul>
           </div>
@@ -40,7 +46,7 @@ const Footer = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
