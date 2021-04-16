395cc79b828cf730b4f1b7e8efd7e7c081557277
Juan Jose Peralta <jperalta@pixel2html.net>
2021-04-16 10:19:02 -0300

    [WIP]

--- src/secciones/home/spa.js
+++ src/secciones/home/spa.js
@@ -8,88 +8,27 @@ const Spa = () => {
   return (
     <section className="spa">
       <div className="container">
-        <div className="carousel slide">
-          {/* PRIMERA CARD */}
-          <div className="spa-card">
-            {/* CARD INFO */}
-            <div className="spa-text">
-              <p>
-                Margot és un SPA GASTRONÓMICO, un lugar ideado a la perfección
-                con una propuesta única en Argentina. Producto autóctono y
-                técnicas precisas en un marco de estilo y confort.
-              </p>
-            </div>
-            <div className="spa-info">
-              <div className="spa-img">
-                <img src={img} alt="Rodrigo Calderón"></img>
-              </div>
-              <div className="data">
-                <p className="name">Rodrigo Calderón</p>
-                <p className="job">Sommelier</p>
-              </div>
-            </div>
-            {/* CARD CONTROLS */}
-            <div className="controls">
-              <img src={icon_back} alt="icon-back"></img>
-              <img src={icon_next} alt="icon-next"></img>
-            </div>
-          </div>
+        {/* Testimonio 1:start */}
+        <div className="testimonio">
+          <p className="testimonio__frase">
+            Margot és un SPA GASTRONÓMICO, un lugar ideado a la perfección con
+            una propuesta única en Argentina. Producto autóctono y técnicas
+            precisas en un marco de estilo y confort.
+          </p>
+
+          <img
+            className="testimonio__imagen"
+            src={img}
+            alt="Rodrigo Calderón"
+          />
+
+          <p className="testimonio__autor">
+            <span>Rodrigo Calderón</span>
+            <div className="separador"></div>
+            Sommelier
+          </p>
         </div>
-        <div className="carousel slide">
-          {/* PRIMERA CARD */}
-          <div className="spa-card">
-            {/* CARD INFO */}
-            <div className="spa-text">
-              <p>
-                La fermentación coloca a los alimentos en un lugar único entre
-                lo crudo y lo cocido. Margot produce e incorpora alimentos
-                fermentados a su carta, dejando a sus platos en ese lugar único
-                que solo los microorganismos saben hacer. Curiosidad e
-                innovación con conocimiento.
-              </p>
-            </div>
-            <div className="spa-info">
-              <div className="spa-img">
-                <img src={img2} alt="Gabriel Vinderola"></img>
-              </div>
-              <div className="data">
-                <p className="name">dr. Gabriel Vinderola</p>
-                <p className="job">Investigador Principal CONICET</p>
-              </div>
-            </div>
-            {/* CARD CONTROLS */}
-            <div className="controls">
-              <img src={icon_back} alt="icon-back"></img>
-              <img src={icon_next} alt="icon-next"></img>
-            </div>
-          </div>
-        </div>
-        <div className="carousel slide">
-          {/* PRIMERA CARD */}
-          <div className="spa-card">
-            {/* CARD INFO */}
-            <div className="spa-text">
-              <p>
-                One of the coolest places in Santa Fe. Thank you for delicious,
-                unique menu and hospitality 💚 Much love friends 😍
-              </p>
-            </div>
-            <div className="spa-info">
-              <div className="spa-img">
-                <img src={img3} alt="Miss monique"></img>
-              </div>
-              <div className="data">
-                <p className="name">Miss monique</p>
-                <p className="job">DJ</p>
-              </div>
-            </div>
-            {/* CARD CONTROLS */}
-            <div className="controls">
-              <img src={icon_back} alt="icon-back"></img>
-              <img src={icon_next} alt="icon-next"></img>
-            </div>
-          </div>
-        </div>
+        {/* Testimonio 1:end */}
       </div>
     </section>
   );
