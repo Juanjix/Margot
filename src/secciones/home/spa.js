40e0544b794ff3834fe9ef1a22d0551da9fc48e0
Tom <tomas@indicius.com>
2021-04-16 10:34:10 -0300

    Home / Testimonios / Agregamos la card para mobile

--- src/secciones/home/spa.js
+++ src/secciones/home/spa.js
@@ -1,8 +1,8 @@
-import img from "../../assets/images/spa/Rodrigo.jpg";
-import img2 from "../../assets/images/spa/Gabriel.jpg";
-import img3 from "../../assets/images/spa/Monique.jpg";
-import icon_back from "../../assets/icons/Icons/Arrow/Back.svg";
-import icon_next from "../../assets/icons/Icons/Arrow/Next.svg";
+import img from '../../assets/images/spa/Rodrigo.jpg'
+import img2 from '../../assets/images/spa/Gabriel.jpg'
+import img3 from '../../assets/images/spa/Monique.jpg'
+import icon_back from '../../assets/icons/Icons/Arrow/Back.svg'
+import icon_next from '../../assets/icons/Icons/Arrow/Next.svg'
 
 const Spa = () => {
   return (
@@ -10,20 +10,12 @@ const Spa = () => {
       <div className="container">
         {/* Testimonio 1:start */}
         <div className="testimonio">
-          <p className="testimonio__frase">
-            Margot és un SPA GASTRONÓMICO, un lugar ideado a la perfección con
-            una propuesta única en Argentina. Producto autóctono y técnicas
-            precisas en un marco de estilo y confort.
-          </p>
+          <p className="testimonio__frase font-weight--500">Margot és un SPA GASTRONÓMICO, un lugar ideado a la perfección con una propuesta única en Argentina. Producto autóctono y técnicas precisas en un marco de estilo y confort.</p>
 
-          <img
-            className="testimonio__imagen"
-            src={img}
-            alt="Rodrigo Calderón"
-          />
+          <img className="testimonio__imagen" src={img} alt="Rodrigo Calderón" />
 
-          <p className="testimonio__autor">
-            <span>Rodrigo Calderón</span>
+          <p className="testimonio__autor color--olivo">
+            <span className="font-weight--600">Rodrigo Calderón</span>
             <div className="separador"></div>
             Sommelier
           </p>
@@ -31,7 +23,7 @@ const Spa = () => {
         {/* Testimonio 1:end */}
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Spa;
+export default Spa
