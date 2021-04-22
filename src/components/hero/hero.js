ae93dbaa9fad6339d23a9d0be60ed413f78ba497
Juan Jose Peralta <jperalta@pixel2html.net>
2021-04-22 10:02:12 -0300

    En hero agregue las imagenes y el carousel

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,13 +1,35 @@
 import img from "../../assets/images/hero/Img.jpg";
-
+import img2 from "../../assets/images/hero/img2.jpg";
+import img3 from "../../assets/images/hero/img3.jpg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 const Hero = () => {
+  var settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    responsive: [
+      { breakpoint: 768, settings: { slidesToShow: 1 } },
+      { breakpoint: 1024, settings: { slidesToShow: 1 } },
+      { breakpoint: 10000, settings: { slidesToShow: 3 } },
+    ],
+  };
   return (
     <div className="hero">
       <div className="container">
         <h2 className="hero__title">
           En cada plato,<br></br>una nueva emoción
         </h2>
-        <img src={img} alt="plato"></img>
+        <Slider {...settings}>
+          <img src={img} alt="plato"></img>
+          <img src={img2} alt="plato"></img>
+          <img src={img3} alt="plato"></img>
+        </Slider>
         <p className="hero__paragraph">
           EN MARGOT BUSCAMOS QUE CADA EXPERIENCIA SEA ÚNICA
         </p>
