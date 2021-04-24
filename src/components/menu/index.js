95ba4c1c1d9b3bdd7914add54adc4499a83f2b29
Tom <tomas@indicius.com>
2021-04-24 15:21:14 -0300

    Components / Menu / Agregue 'Menu Toggler'

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,32 +1,37 @@
-import logo from "../../assets/images/menu/Logo.svg";
+import react, { useState } from 'react'
+
+import logo from '../../assets/images/menu/Logo.svg'
+
 const Nav = () => {
-  function changeMenu() {}
+  const [activo, setActivo] = useState(false)
+
+  function toggleMenu() {
+    setActivo(!activo)
+  }
+
   return (
     <div className="menu">
       <div className="container">
         <div className="menu__logo">
           <img src={logo} alt="logo"></img>
-          <div className="menu__hamburger" onClick={changeMenu}>
+
+          <div className="menu__hamburger" onClick={toggleMenu}>
             <div className="bar1"></div>
             <div className="bar2"></div>
             <div className="bar3"></div>
           </div>
         </div>
-        <div className="menu__info" id="myDIV">
+
+        <div className={activo ? 'menu__info activo' : 'menu__info'}>
           <button className="button button__primary">hacer reserva</button>
-          <p>
-            Talcahuano 6960 (S3004JEL) Santa Fe, Provincia de Santa Fe,
-            Argentina
-          </p>
+          <p>Talcahuano 6960 (S3004JEL) Santa Fe, Provincia de Santa Fe, Argentina</p>
           <br></br>
           <p>+54 9 3425 66-2175</p>
           <br></br>
           <p>contacto@margot.com</p>
           <ul className="menu__media">
             <li>
-              <a href="https://www.instagram.com/margot_e.c/?hl=en">
-                INSTAGRAM
-              </a>
+              <a href="https://www.instagram.com/margot_e.c/?hl=en">INSTAGRAM</a>
             </li>
             <li>
               <a href="https://www.facebook.com/margotrestostafe/">FACEBOOK</a>
@@ -35,7 +40,7 @@ const Nav = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Nav;
+export default Nav
