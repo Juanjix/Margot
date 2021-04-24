a06252cb679a111e5d2818c11131fad24cce518c
Tom <tom@roselab.co>
2021-04-24 18:18:18 -0300

    Home / Testimonials / Agregue flechas para mobile

--- src/secciones/home/testimonials.js
+++ src/secciones/home/testimonials.js
@@ -1,37 +1,50 @@
-import img from "../../assets/images/spa/Rodrigo.jpg";
+import img from '../../assets/images/spa/Rodrigo.jpg'
 // import img2 from "../../assets/images/spa/Gabriel.jpg";
 // import img3 from "../../assets/images/spa/Monique.jpg";
-// import icon_back from "../../assets/icons/Icons/Arrow/Back.svg";
-// import icon_next from "../../assets/icons/Icons/Arrow/Next.svg";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import { ReactComponent as IconBack } from '../../assets/icons/Back.svg'
+import { ReactComponent as IconNext } from '../../assets/icons/Next.svg'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slider from 'react-slick'
+
+const ArrowBack = (props) => {
+  return (
+    <button className={props.className} onClick={props.onClick}>
+      <IconBack />
+    </button>
+  )
+}
+
+const ArrowNext = (props) => {
+  return (
+    <button className={props.className} onClick={props.onClick}>
+      <IconNext />
+    </button>
+  )
+}
 
 const Testimonials = () => {
   var settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
-    slidesToScroll: 3,
+    prevArrow: <ArrowBack />,
+    nextArrow: <ArrowNext />
     // responsive: [
     //   { breakpoint: 768, settings: { slidesToShow: 1 } },
     //   { breakpoint: 1024, settings: { slidesToShow: 1 } },
     //   { breakpoint: 10000, settings: { slidesToShow: 3 } },
     // ],
-  };
+  }
   return (
     <section className="spa">
       <div className="container">
-        <Slider {...settings}>
+        <Slider {...settings} className="testimonials__carousel">
           {/* Testimonio 1:start */}
           <div className="testimonio">
             <div className="testimonio__frase font-weight--500">
-              <p>
-                Margot és un SPA GASTRONÓMICO, un lugar ideado a la perfección
-                con una propuesta única en Argentina. Producto autóctono y
-                técnicas precisas en un marco de estilo y confort.
-              </p>
+              <p>Margot és un SPA GASTRONÓMICO, un lugar ideado a la perfección con una propuesta única en Argentina. Producto autóctono y técnicas precisas en un marco de estilo y confort.</p>
 
               <p className="testimonio__autor d-none d-md-flex align-items-center color--olivo">
                 <span className="font-weight--600">Rodrigo Calderón</span>
@@ -41,11 +54,7 @@ const Testimonials = () => {
               </p>
             </div>
 
-            <img
-              className="testimonio__imagen"
-              src={img}
-              alt="Rodrigo Calderón"
-            />
+            <img className="testimonio__imagen" src={img} alt="Rodrigo Calderón" />
 
             <p className="testimonio__autor d-md-none color--olivo">
               <span className="font-weight--600">Rodrigo Calderón</span>
@@ -56,11 +65,7 @@ const Testimonials = () => {
           {/* Testimonio 1:start */}
           <div className="testimonio">
             <div className="testimonio__frase font-weight--500">
-              <p>
-                Margot és un SPA GASTRONÓMICO, un lugar ideado a la perfección
-                con una propuesta única en Argentina. Producto autóctono y
-                técnicas precisas en un marco de estilo y confort.
-              </p>
+              <p>Margot és un SPA GASTRONÓMICO, un lugar ideado a la perfección con una propuesta única en Argentina. Producto autóctono y técnicas precisas en un marco de estilo y confort.</p>
 
               <p className="testimonio__autor d-none d-md-flex align-items-center color--olivo">
                 <span className="font-weight--600">Rodrigo Calderón</span>
@@ -70,11 +75,7 @@ const Testimonials = () => {
               </p>
             </div>
 
-            <img
-              className="testimonio__imagen"
-              src={img}
-              alt="Rodrigo Calderón"
-            />
+            <img className="testimonio__imagen" src={img} alt="Rodrigo Calderón" />
 
             <p className="testimonio__autor d-md-none color--olivo">
               <span className="font-weight--600">Rodrigo Calderón</span>
@@ -85,6 +86,6 @@ const Testimonials = () => {
         </Slider>
       </div>
     </section>
-  );
-};
-export default Testimonials;
+  )
+}
+export default Testimonials
