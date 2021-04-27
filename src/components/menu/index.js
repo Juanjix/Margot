3236110080b3c948c62e3e9b1a32d2ecba0d4992
Tom <tomas@indicius.com>
2021-04-27 07:18:16 -0300

    Components / Menu / Agregamos 'SubMenu'

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -31,6 +31,51 @@ const Menu = () => {
       <a href="/" className="menu__button-reserva d-none d-md-flex button button__primary">
         Hacer Reserva
       </a>
+
+      <div className={activo ? 'menu__content menu__content--activo' : 'menu__content'}>
+        <div className="footer">
+          <div className="container">
+            <div className="row justify-content-between text-center text-sm-left">
+              <div className="menu__reserva d-sm-flex flex-wrap col-12 col-sm-6">
+                <div>
+                  <a className="button button__primary" href="/">
+                    Hacer reserva
+                  </a>
+                </div>
+
+                <div className="menu__links-lenguajes d-none d-sm-flex align-items-center align-self-end">
+                  <a href="/">Es</a>
+                  <div className="separador"></div>
+                  <a href="/en">En</a>
+                </div>
+              </div>
+              <div className="direccion col-12 col-sm-6 col-md-5">
+                <div>
+                  <p>Talcahuano 6960 (S3004JEL) Santa Fe, Provincia de Santa Fe, Argentina</p>
+                  <p>+54 9 3425 66-2175</p>
+                  <p>contacto@margot.com</p>
+                </div>
+                <div>
+                  <ul className="media button__secondary justify-content-center justify-content-sm-start">
+                    <li>
+                      <a href="https://www.instagram.com/margot_e.c/?hl=en">INSTAGRAM</a>
+                    </li>
+                    <li>
+                      <a href="https://www.facebook.com/margotrestostafe/">FACEBOOK</a>
+                    </li>
+                  </ul>
+
+                  <div className="menu__links-lenguajes d-flex d-sm-none align-items-center justify-content-center">
+                    <a href="/">Es</a>
+                    <div className="separador"></div>
+                    <a href="/en">En</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
