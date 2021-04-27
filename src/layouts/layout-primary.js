eb665e0b58ef80bdad96e6fed4d39cfc04b26cc0
Tom <tom@roselab.co>
2021-04-27 08:45:25 -0300

    General / Agregue informacion SEO

--- src/layouts/layout-primary.js
+++ src/layouts/layout-primary.js
@@ -1,19 +1,8 @@
-// Libraries
-import Helmet from 'react-helmet'
-
 // Styles
 import '../assets/styles/main.scss'
 
 const Layout = (props) => {
-  return (
-    <main>
-      <Helmet>
-        <title>Margot</title>
-        <meta name="description" content="Amamos lo que hacemos e invertimos todo nuestro tiempo y energía en una constante y perpetua búsqueda de la perfección. Por eso diseñamos un espacio de exploración gastronómica donde ofrecemos cocina auténtica e innovadora basada en productos locales de calidad." />
-      </Helmet>
-      {props.children}
-    </main>
-  )
+  return <main>{props.children}</main>
 }
 
 export default Layout
