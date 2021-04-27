7f26b551cab7387dd216e7f43387e523688188cd
Tom <tomas@indicius.com>
2021-04-26 21:07:48 -0300

    Components / Menu / Refactoreamos la navegacion

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,62 +1,38 @@
-import { useState } from "react";
-//Logo
-import logo from "../../assets/images/menu/Logo.svg";
+import { useState } from 'react'
 
-const Nav = () => {
-  const [activo, setActivo] = useState(false);
+//Logo
+import logo from '../../assets/images/menu/Logo.svg'
+
+const Menu = () => {
+  const [activo, setActivo] = useState(false)
 
   const toggleMenu = () => {
-    setActivo(!activo);
-  };
+    setActivo(!activo)
+  }
 
   return (
-    <div className="menu">
-      <div className="container">
-        <div className="menu__logo">
-          <img src={logo} alt="logo"></img>
-
-          <div className="menu__hamburger" onClick={toggleMenu}>
-            <div className="bar1"></div>
-            <div className="bar2"></div>
-            <div className="bar3"></div>
-          </div>
-        </div>
-
-        <div className={activo ? "menu__info activo" : "menu__info"}>
-          <div className="row justify-content-evenly">
-            <div className="col-12 col-sm-6 col-md-5">
-              <button className="button button__primary">HACER RESERVA</button>
-            </div>
-
-            <div className="direccion col-12 col-sm-6 col-md-3">
-              <div>
-                <p>
-                  Talcahuano 6960 (S3004JEL) Santa Fe, Provincia de Santa Fe,
-                  Argentina
-                </p>
-                <p>+54 9 3425 66-2175</p>
-                <p>contacto@margot.com</p>
-              </div>
-              <div>
-                <ul className="menu__media">
-                  <li>
-                    <a href="https://www.instagram.com/margot_e.c/?hl=en">
-                      INSTAGRAM
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.facebook.com/margotrestostafe/">
-                      FACEBOOK
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+    <nav className="menu">
+      <div className="menu__links-lenguajes d-none d-md-flex align-items-center">
+        <a href="/">Es</a>
+        <div className="separador"></div>
+        <a href="/en">En</a>
       </div>
-    </div>
-  );
-};
 
-export default Nav;
+      <a className="menu__logo" href="/" title="Logo">
+        <img src={logo} alt="Margot" />
+      </a>
+
+      <button className={activo ? 'menu__toggler menu__toggler--activo' : 'menu__toggler'} type="button" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <a href="/" className="menu__button-reserva d-none d-md-flex button button__primary">
+        Hacer Reserva
+      </a>
+    </nav>
+  )
+}
+
+export default Menu
